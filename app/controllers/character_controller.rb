54d203a4d1eca006c4b8ac8c95b84a52b304d3fa
Craig_Donavin <crdonavin@gmail.com>
2019-08-12 21:18:04 -0500

    Pseudo code forms

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,3 +1,19 @@
 class CharacterController < ApplicationController
 
+    get '/character' do
+        # shows a list of chars, select one to see or edit stats
+        erb :'character/show'
+    end
+
+    get '/character/new' do
+        # creates form that where the primary stats are made
+        # redirects to stat creation
+        # saves primary, assigns stats to character
+        erb :'/character/new'
+    end
+
+    get 'character/:id/edit' do
+        # similar to new but edit field contain entire form, not in separate parts
+        erb :'/character/edit'
+    end
 end
